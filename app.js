e2439ba169c5fb2f0f23a50ad189a68c7b34d591
Nabanita Bag <ashley.makruz@gmail.com>
2021-03-30 00:40:32 +0530

    used env variables

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 //jshint esversion:6
+require('dotenv').config();
 const express = require("express"),
       bodyparser = require("body-parser"),
       ejs = require("ejs"),
@@ -20,7 +21,7 @@ const userSchema = new mongoose.Schema({
   password: String
 });
 
-const secret = "Thisisourlittlesecret";
+const secret = process.env.SECRET;
 userSchema.plugin(encrypt, {secret: secret, encryptedFields:["password"]});
 
 const User = new mongoose.model("User", userSchema);
